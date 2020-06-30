4978a8ca96f2f169acbc69f4211fdadfcd0fa0c4
Sara Venkatraman <venkatraman.sara@gmail.com>
2020-06-29 22:14:07 -0400

    Identified neighbors of differentially-expressed genes

--- DatasetConstruction.R
+++ DatasetConstruction.R
@@ -61,3 +61,79 @@ stringDBMatrix <- spread(stringDBexp, gene2, experimental)
 rownames(stringDBMatrix) <- stringDBMatrix$gene1
 stringDBMatrix <- stringDBMatrix[,-1]
 write.csv(stringDBMatrix, file="Dme_STRING_exp_matrix.csv")
+
+# ------------------------------------------------------------------
+
+# Create a CSV dataset of differentially-expressed genes and their neighbors as
+# identified by composite STRING-DB scores. To run this code, first run the functions 
+# Gene.Name.To.Flybase.ID and Flybase.ID.To.Gene.Name in EmpiricalBayesFunctions.R.
+
+# Read gene names and Flybase IDs (converted to character) and read composite STRING scores
+geneNames <- read.csv("../Processed Data for Clustering/DEgeneNames.csv", header=T)
+geneIDs <- read.csv("../Processed Data for Clustering/DEgeneIDs.csv", header=T)  
+geneNames <- as.character(geneNames$geneName)
+geneIDs <- as.character(geneIDs$geneID)
+stringDBMatrix <- read.csv("../Drosophila Data (Processed)/Dme_STRING_matrix.csv", header=T, row.names=1)
+
+# Initialize vector for storing names of neighbors not in the DE gene set and the number
+# of new additions (i.e. length of DEgenesNeighbors)
+DEgenesNeighbors <- c()
+numNewNeighbors <- 0
+
+# For each DE gene, find its row index in the STRING matrix, then search that row for other
+# genes with which it is associated (that are not already DE genes)
+for(i in 1:length(geneNames)) {
+  # Find Flybase ID of current gene in STRING-DB matrix 
+  index <- match(Gene.Name.To.Flybase.ID(geneNames[i]), rownames(stringDBMatrix))
+  
+  # If the index is not NA, i.e. if the current gene is represented in the matrix, then...
+  if(!is.na(index)) {
+    # Get the IDs of the associated genes
+    STRINGscores <- stringDBMatrix[index,]
+    
+    # Find the column indices of genes scores were >= 200
+    associatedIndices <- which(STRINGscores >= 900)
+    associatedIDs <- colnames(stringDBMatrix)[associatedIndices]
+    
+    # Find which of the associated genes are already in the set of DE genes
+    notDEgenes <- !(associatedIDs %in% geneIDs) # indicates whether or not each gene is DE
+    indicesOfNeighbors <- which(notDEgenes)
+    neighborIDs <- associatedIDs[indicesOfNeighbors]
+    numAdditions <- length(indicesOfNeighbors)
+    if(numAdditions > 0) {
+      DEgenesNeighbors[(numNewNeighbors+1):(numNewNeighbors+numAdditions)] <- neighborIDs
+    }
+    numNewNeighbors <- length(DEgenesNeighbors)
+  }
+}
+
+DEgenesNeighbors <- unique(DEgenesNeighbors)
+
+# How many new neighbors are added?
+# - With STRING score threshold of >= 200, we add 9328 new genes to the set of 951.
+# - With STRING score threshold of >= 500, we add 5607 new genes.
+# - With STRING score threshold of >= 700, we add 3539 new genes.
+# - With STRING score threshold of >= 900, we add 2147 new genes (2013 after excluding NAs).
+
+# Construct a dataset containing the log-fold change expression values from both the
+# differentially-expressed genes and the neighbors
+
+# Load the full dataset from the experiment
+load("../Drosophila Data (Processed)/full_results.Rdata")
+
+# Set row names of full dataset to Flybase ID for easier access to certain rows
+rownames(full_results) <- full_results$gene_ID
+
+# Extract the expression data for the neighbor genes identified above
+geneDataNewNeighbors <- full_results[DEgenesNeighbors,]
+
+# Exclude rows with missing data
+geneDataNewNeighbors <- geneDataNewNeighbors[complete.cases(geneDataNewNeighbors),]
+
+# Set row names back to gene names and save only the log-fold change data from early timeframe
+rownames(geneDataNewNeighbors) <- geneDataNewNeighbors$gene_name
+geneDataNewNeighbors <- geneDataNewNeighbors[,3:19]
+
+# Write to CSV
+write.csv(geneDataNewNeighbors, "../Processed Data for Clustering/LogChangeDENeighbors900.csv")
+
