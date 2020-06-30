1f9ba84bdd0cd38b941d541bd103f8bf3bee7d64
Sara Venkatraman <venkatraman.sara@gmail.com>
2020-06-30 11:28:21 -0400

    Subset sampled from both DE genes and neighbor genes

--- DatasetLoader.R
+++ DatasetLoader.R
@@ -1,6 +1,6 @@
 # --- Script 2: Read data ---
 
-# Note: "Processed Data for Clustering" contains the following subdirectories. The files
+# "Processed Data for Clustering" contains the following subdirectories. The files
 # in each subdirectory have the same names.
 # - "/Differentially-Expressed/" only contains gene data of differentially-expressed genes
 # - "/Neighbors (STRING > 900)/" only contains gene data for the "neighbors" (identified 
@@ -23,6 +23,13 @@ geneData <- read.csv(paste("../Processed Data for Clustering/", subdirectory, "/
 geneNames <- as.character(geneNames$geneName)
 geneIDs <- as.character(geneIDs$geneID)
 
-# Define a subset of genes on which to run the R^2 analysis
-subsetSize <- 250
-genesSubset <- sample(geneNames, size=subsetSize)
+# --- Subset selection: adjust as needed depending on choice of subdirectory ---
+# Define a subset of genes on which to run the R^2 analysis: half of the subset is 
+# differentially-expressed, the other half is neighbors
+DEgeneNames <- read.csv("../Processed Data for Clustering/Differentially-Expressed/GeneNames.csv")
+neighborGeneNames <- read.csv("../Processed Data for Clustering/Neighbors (STRING > 900)/GeneNames.csv")
+
+subsetSize <- 200
+DEsubset <- as.character(sample(DEgeneNames$geneName, size=0.5*subsetSize))
+neighborSubset <- as.character(sample(neighborGeneNames$geneName, size=0.5*subsetSize))
+genesSubset <- unique(c(DEsubset, neighborSubset)) # There should be no duplicates anyway
