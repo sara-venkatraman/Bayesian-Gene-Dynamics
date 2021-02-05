c6b91ef94d8df90d05390c879aee022d9ec26f69
Sara Venkatraman <venkatraman.sara@gmail.com>
2021-02-04 22:12:26 -0500

    Posterior edge probabilities

--- 3_Results.R
+++ 3_Results.R
@@ -61,4 +61,46 @@ Draw.R2.Scatterplot(nonBayesLLR2Mat.other, nonBayesLLR2Mat - nonBayesLLR2Mat.own
 Draw.R2.Scatterplot(bayesLLR2Mat.other, bayesLLR2Mat - bayesLLR2Mat.own, 
                     priorMatrix, geneSubset)
 
+# --- Inferring R^2 cutoffs from data ---
 
+# Vectorize the LLR2 matrix and the prior matrix (saved into CSV)
+bayesLLR2Vec <- data.frame(Vectorize.Labeled.Square.Matrix(bayesLLR2Mat),
+                           Vectorize.Labeled.Square.Matrix(priorMatrix))
+bayesLLR2Vec <- bayesLLR2Vec[order(bayesLLR2Vec[,1], decreasing=T),]
+colnames(bayesLLR2Vec) <- c("LLR2", "Prior")
+
+# How many edges exist are there in the prior matrix? Compute the proportion
+# of "1"s in one of the triangular halves of priorMatrix
+numPositiveEdges <- sum(priorMatrix[upper.tri(priorMatrix)] == 1, na.rm=T)
+numPossibleEdges <- sum(upper.tri(priorMatrix))
+priorEdgeProb <- numPositiveEdges / numPossibleEdges 
+
+# It's about 3.3%. Let prior parameters for Beta distribution on p be
+# alpha = 1 and beta = 29.7. Then the posterior mean is:
+alpha <- 1;  beta <- 29.7
+postEdgeProb <- (numPositiveEdges + alpha) / (numPossibleEdges + alpha + beta)
+
+# Take the top postEdgeProb proportion of R^2 values
+topConnections <- bayesLLR2Vec[1:round(postEdgeProb*length(bayesLLR2Vec))]
+topGenePairs <- strsplit(names(topConnections), ", ")
+Plot.Gene.Group(topGenePairs[[50]])
+
+# Compute P(W=1 | R^2 = r). Rather than using the built-in 'density' function,
+# use the kdensity package, which returns a density that can be evaluated at a point
+library(kdensity)
+LLR2WithPrior0 <- bayesLLR2Vec[is.na(bayesLLR2Vec$Prior) | bayesLLR2Vec$Prior == 0,]
+LLR2WithPrior1 <- bayesLLR2Vec[!is.na(bayesLLR2Vec$Prior) & bayesLLR2Vec$Prior == 1,]
+LLR2Density0 <- kdensity(LLR2WithPrior0$LLR2)
+LLR2Density1 <- kdensity(LLR2WithPrior1$LLR2)
+plot(LLR2Density0, main="Density of LLR2 (W=0)")
+plot(LLR2Density1, main="Density of LLR2 (W=1)")
+Compute.Posterior.Edge.Prob <- function(R2Density0, R2Density1, r, w, priorEdge1Prob) {
+  priorEdge0Prob <- 1 - priorEdge1Prob
+  denominator <- R2Density0(r)*priorEdge0Prob + R2Density1(r)*priorEdge1Prob
+  if(w == 0)
+    numerator <- R2Density0(r)*priorEdge0Prob
+  else
+    numerator <- R2Density1(r)*priorEdge1Prob
+  numerator/denominator
+}
+Compute.Posterior.Edge.Prob(LLR2Density0, LLR2Density1, 0.5, 1, priorEdgeProb)
